ed5bc01b490dcdcc23a9d483aece69c857bdeaca
Makarov Andrey <xabor@yandex.ru>
2022-07-14 19:46:27 +0300

    Создана структура базы данных и админка 14072022

--- roomsmeters/admin.py
+++ roomsmeters/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 from . import models
 
+
+@admin.register(models.Watermeter)
+class WatermeterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Electricitymeter)
+class ElectricitymeterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Renter)
+class RenterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Waterdate)
+class WaterdateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Electricitydate)
+class ElectricitydateAdmin(admin.ModelAdmin):
+    pass
--- roomsmeters/models.py
+++ roomsmeters/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 
 class Watermeter(models.Model):
-    name = models.CharField(max_length=100)
-    serialnumber = models.CharField(max_length=30, primary_key=True, null=False, blank=False)
-    verificationdate = models.DateField(auto_now=True)
+    name = models.CharField(max_length=100, verbose_name="Название")
+    serialnumber = models.CharField(max_length=30, primary_key=True, null=False, blank=False, verbose_name="Серийный номер")
+    verificationdate = models.DateField(blank=True, verbose_name="Дата поверки")
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         ordering = ['-serialnumber']
@@ -13,9 +16,18 @@ class Watermeter(models.Model):
 
 
 class Electricitymeter(models.Model):
-    name = models.CharField(max_length=100)
-    serialnumber = models.CharField(max_length=30, primary_key=True, null=False, blank=False)
-    verificationdate = models.DateField(auto_now=True)
+    name = models.CharField(max_length=100, verbose_name="Название")
+    serialnumber = models.CharField(max_length=30, primary_key=True, null=False, blank=False, verbose_name="Серийный номер")
+    verificationdate = models.DateField(blank=True, verbose_name="Дата поверки")
+    ratio_transform = models.PositiveSmallIntegerField(null=False, blank=False, default=1,
+                                                       verbose_name="Коэффициент трансформации")
+    ratio_loss = models.PositiveSmallIntegerField(null=False, blank=False, default=1.2,
+                                                  verbose_name="Коэффициент потерь")
+
+
+
+    def __str__(self):
+        return self.name
 
 
     class Meta:
@@ -25,11 +37,19 @@ class Electricitymeter(models.Model):
 
 
 class Renter(models.Model):
-    name = models.CharField(max_length=100)
-    iswater = models.BooleanField(blank=False, default=False)
-    watermeter = models.ForeignKey(Watermeter, blank=True, null=True, on_delete=models.CASCADE)
-    iselectricity = models.BooleanField(blank=False, default=True)
-    electricitymeter = models.ForeignKey(Electricitymeter, blank=True, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name="Название арендатора")
+    iswater = models.BooleanField(blank=False, default=False, verbose_name="Есть вода?")
+    watermeter = models.ForeignKey(Watermeter, blank=True, null=True, on_delete=models.CASCADE,
+                                   verbose_name="Счетчик воды")
+    tarifofwater = models.PositiveSmallIntegerField(blank=False, null=False, default=9, verbose_name="Тариф вода")
+    iselectricity = models.BooleanField(blank=False, default=True, verbose_name="Есть электроэнергия?")
+    electricitymeter = models.ForeignKey(Electricitymeter, blank=True, null=True, on_delete=models.CASCADE,
+                                         verbose_name="Счетчик электроэнергии")
+    tarifofelectricity = models.PositiveSmallIntegerField(blank=False, null=False, default=11.8,
+                                                          verbose_name="Тариф за электроэнергию")
+
+    def __str__(self):
+        return self.name
 
 
     class Meta:
@@ -39,11 +59,14 @@ class Renter(models.Model):
 
 
 class Room(models.Model):
-    name = models.CharField(max_length=100)
-    roomsquare = models.SmallIntegerField(blank=False)
-    building = models.CharField(max_length=10)
-    floor = models.CharField(max_length=10)
-    renter = models.ForeignKey(Renter, blank=True, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name="Номер(название) помещения")
+    roomsquare = models.PositiveSmallIntegerField(blank=False, verbose_name="Площадь помещения")
+    building = models.CharField(max_length=10, verbose_name="Корпус")
+    floor = models.CharField(max_length=10, verbose_name="Этаж")
+    renter = models.ForeignKey(Renter, blank=True, null=True, on_delete=models.CASCADE, verbose_name="Арендатор")
+
+    def __str__(self):
+        return self.name
 
 
     class Meta:
@@ -53,15 +76,35 @@ class Room(models.Model):
 
 
 class Waterdate(models.Model):
-    serialnumber = models.ForeignKey(Watermeter, blank=True, null=True, on_delete=models.CASCADE)
-    datedata = models.DateField(auto_now=True, null=False, blank=False)
-    consumption = models.SmallIntegerField(blank=False)
+    serialnumber = models.ForeignKey(Watermeter, blank=True, null=True, on_delete=models.CASCADE,
+                                     verbose_name="Серийный номер")
+    datedata = models.DateField(auto_now=True, null=False, blank=False, verbose_name="Дата снятия показаний")
+    consumption = models.PositiveSmallIntegerField(blank=False, verbose_name="Текущее показание")
+
+    def __str__(self):
+        return self.serialnumber
+
+
+    class Meta:
+        ordering = ['-serialnumber']
+        verbose_name_plural = 'Показания счетчиков воды'
+        verbose_name = 'Показание счетчика вода'
 
 
 class Electricitydate(models.Model):
-    serialnumber = models.ForeignKey(Electricitymeter, blank=True, null=True, on_delete=models.CASCADE)
-    datedata = models.DateField(auto_now=True, null=False, blank=False)
-    consumption = models.SmallIntegerField(blank=False)
+    serialnumber = models.ForeignKey(Electricitymeter, blank=True, null=True, on_delete=models.CASCADE,
+                                     verbose_name="Серийный номер")
+    datedata = models.DateField(null=False, blank=False, verbose_name="Дата снятия показаний")
+    consumption = models.PositiveSmallIntegerField(blank=False, verbose_name="Текущие показание")
+
+    def __str__(self):
+        return self.serialnumber
+
+
+    class Meta:
+        ordering = ['-serialnumber']
+        verbose_name_plural = 'Показания счетчиков электроэнергии'
+        verbose_name = 'Показание счетчика электроэнергии'
 
 
 
